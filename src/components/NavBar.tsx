f5a364afccd66def58e82faba30edc70d234fed2
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-05-03 17:08:22 +0200

    style: change opacity NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 
 export default function NavBar() {
    return (
-      <menu className="bg-black/50 text-white relative z-10">
-         <ul className="flex justify-center gap-10 border-b-[1px] border-t-[1px] border-yellow-600">
+      <menu className="bg-black/60 text-white relative z-10">
+         <ul className="flex justify-center gap-12 border-b-[1px] border-t-[1px] border-yellow-600">
             {navItems.map((item) => (
                <Link
-                  className="hover:text-white text-white/50 transition-all duration-300 py-4"
+                  className="hover:text-white text-white/70 transition-all duration-300 py-4"
                   key={item.text}
                   href={item.route}
                >
