7a65d3e1a5b38b4f4d3683aa1fdf8498f5bc9979
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-05-12 16:14:41 +0200

    feat: change favicon.ico

--- src/data/filterLanguage.ts
+++ src/data/filterLanguage.ts
@@ -1,8 +1,7 @@
 import { Repository } from './types';
 
-const filterJavaScript = (repo: Repository) =>
-   repo.language === 'JavaScript' && repo.size > 175 && repo.created_at > '2022-09-01';
+const filterJavaScript = (repo: Repository) => repo.language === 'JavaScript' && repo.created_at > '2022-09-01';
 const filterTypeScript = (repo: Repository) =>
-   repo.language === 'TypeScript' && repo.size > 175 && repo.created_at > '2022-09-01';
+   repo.language === 'TypeScript' && repo.size > 100 && repo.created_at > '2022-09-01';
 
 export { filterJavaScript, filterTypeScript };
